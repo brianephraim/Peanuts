577af6a4a2fb6961d90d5ba7c30c53c0b8aadb93
Brian Ephraim <brianephraim@gmail.com>
2013-03-28 11:55:21 -0400

    pre find and replace

--- bladestest.js
+++ bladestest.js
@@ -24,32 +24,7 @@ if (Meteor.isClient) {
       this.frag = frag;
       this.$el = $el;
     }
-
-    function returnStates(self,nameOfCreated){
-      var dataSets = {
-        tvShowId: self.tvShowObj._id,
-        previousBirthingArray: Session.get('birthingArray'+self.nestedViewItem.viewId+'-'+self.nestedViewItem.viewIdX+'-'+nameOfCreated),
-        previousSelectedArray: Session.get('selectedArray'+self.nestedViewItem.viewId+'-'+self.nestedViewItem.viewIdX+'-'+nameOfCreated),
-        previousDyingArray: Session.get('dyingArray'+self.nestedViewItem.viewId+'-'+self.nestedViewItem.viewIdX+'-'+nameOfCreated),
-      }
-      var states = {
-        previousBirthingArrayContainsId: _.indexOf(dataSets.previousBirthingArray, dataSets.tvShowId) === -1 ? false : true,
-        previousSelectedArrayContainsId: _.indexOf(dataSets.previousSelectedArray, dataSets.tvShowId) === -1 ? false : true,
-        previousDyingArrayContainsId: _.indexOf(dataSets.previousDyingArray, dataSets.tvShowId) === -1 ? false : true,
-
-        previousBirthingArrayExists: typeof dataSets.previousBirthingArray === 'undefined' ? false : true,
-        previousSelectedArrayExists: typeof dataSets.previousSelectedArray === 'undefined' ? false : true,
-        previousDyingArrayExists: typeof dataSets.previousDyingArray === 'undefined' ? false : true,
-
-        setDataArray: function(name,arr){
-          Session.set(name+self.nestedViewItem.viewId+'-'+self.nestedViewItem.viewIdX+'-'+nameOfCreated,arr);
-        }
-      }
-      return $.extend(states, dataSets);
-    }
-    function returnStates2(self){
-      console.log(self)
-      console.log('birthingArray'+self.nestedViewItem2.viewId+'-'+self.nestedViewItem2.viewIdX)
+    function returnStates(self){
       var dataSets = {
         tvShowId: self.tvShowObj._id,
         previousBirthingArray: Session.get('birthingArray'+self.nestedViewItem2.viewId+'-'+self.nestedViewItem2.viewIdX),
@@ -71,44 +46,8 @@ if (Meteor.isClient) {
       }
       return $.extend(states, dataSets);
     }
-    function showHideView2(self){
-      var s = returnStates2(self);
-      //Add to selected when appropriate
-      if(s.previousSelectedArrayExists && !s.previousSelectedArrayContainsId){
-        s.previousSelectedArray.push(s.tvShowId)
-        s.setDataArray('selectedArray',s.previousSelectedArray)
-      }
-      if(!s.previousSelectedArrayExists){
-        s.previousSelectedArray = [s.tvShowId]
-        s.setDataArray('selectedArray',s.previousSelectedArray)
-      }
-
-      //Add to birthing when appropriate
-      if(s.previousBirthingArrayExists && !s.previousBirthingArrayContainsId){
-        s.previousBirthingArray.push(s.tvShowId)
-        s.setDataArray('birthingArray',s.previousBirthingArray)
-      }
-      if(!s.previousBirthingArrayExists){
-        s.setDataArray('birthingArray',[s.tvShowId])
-      }
-
-      //Make an existing characterList disappear
-      if(
-        (s.previousSelectedArrayExists && s.previousSelectedArrayContainsId) && 
-        (!s.previousBirthingArrayExists || !s.previousBirthingArrayContainsId)
-      ){
-        s.previousSelectedArray.splice(_.indexOf(s.previousSelectedArray, s.tvShowId),1)
-        if(s.previousDyingArrayExists && !s.previousDyingArrayContainsId){
-          s.previousDyingArray.push(s.tvShowId)
-        } else {
-          s.previousDyingArray = [s.tvShowId];
-        }
-        s.setDataArray('selectedArray',s.previousSelectedArray)
-        s.setDataArray('dyingArray',s.previousDyingArray)
-      }
-    }
-    function showHideView(self,viewNameWithIndex){
-      var s = returnStates(self,viewNameWithIndex);
+    function showHideView(self){
+      var s = returnStates(self);
       //Add to selected when appropriate
       if(s.previousSelectedArrayExists && !s.previousSelectedArrayContainsId){
         s.previousSelectedArray.push(s.tvShowId)
@@ -158,19 +97,14 @@ if (Meteor.isClient) {
       })();
     }
     function animationEndHideShowCleanup(self,viewNameWithIndex){
-      var nestedViewArray = self.nestedViewItem.nestedViewArray;
-      for(var i = 0, l = nestedViewArray.length; i<l ;i++){
-        if(nestedViewArray[i].viewIdX === viewNameWithIndex){
-          var s = returnStates(self,nestedViewArray[i].viewIdX);
-          if(s.previousBirthingArrayExists && s.previousBirthingArrayContainsId){
-            s.previousBirthingArray.splice(_.indexOf(s.previousBirthingArray, s.tvShowId),1)
-            s.setDataArray('birthingArray',s.previousBirthingArray)
-          }
-          if(s.previousDyingArrayExists && s.previousDyingArrayContainsId){
-            s.previousDyingArray.splice(_.indexOf(s.previousDyingArray, s.tvShowId),1)
-            s.setDataArray('dyingArray',s.previousDyingArray)
-          }
-        }
+      var s = returnStates(self);
+      if(s.previousBirthingArrayExists && s.previousBirthingArrayContainsId){
+        s.previousBirthingArray.splice(_.indexOf(s.previousBirthingArray, s.tvShowId),1)
+        s.setDataArray('birthingArray',s.previousBirthingArray)
+      }
+      if(s.previousDyingArrayExists && s.previousDyingArrayContainsId){
+        s.previousDyingArray.splice(_.indexOf(s.previousDyingArray, s.tvShowId),1)
+        s.setDataArray('dyingArray',s.previousDyingArray)
       }
     }
 
@@ -224,23 +158,31 @@ if (Meteor.isClient) {
       return '-webkit-animation: '+anim.name+' 1s ease;';
     }
 
-    Template.characterList.events = {
+    Template.characterListPanel.events = {
       "webkitAnimationEnd": function (e, tmpl, x) {
         var self = this;
-        animationEndHideShowCleanup(self,'characterList0')
+        console.log(this)
+        animationEndHideShowCleanup(self)
       }
     }
-    Template.genreList.events = {
+    Template.genreListPanel.events = {
       "webkitAnimationEnd": function (e, tmpl, x) {
         var self = this;
-        animationEndHideShowCleanup(self,'genreList1')
+        animationEndHideShowCleanup(self)
       }
     }
-    Template.customButton.events = {
+    Template.genreListButton.events = {
       "click": function (e, tmpl, x) {
         var self = this;
         console.log(this)
-        showHideView2(self)
+        showHideView(self)
+      }
+    }
+    Template.characterListButton.events = {
+      "click": function (e, tmpl, x) {
+        var self = this;
+        console.log(this)
+        showHideView(self)
       }
     }
   //})()
@@ -301,17 +243,6 @@ if (Meteor.isClient) {
             })()
           },
           eventMap: {
-            "click .showCharacters": function (e, tmpl, x) {
-              console.log('click',this)
-              console.log('click e',e)
-              var self = this;
-              showHideView(self,'characterList0')
-            },
-            "click .showGenres": function (e, tmpl, x) {
-              console.log('click',this)
-              var self = this;
-              showHideView(self,'genreList1')
-            },
             "click .changeNameButton": function (e, tmpl, x) {
               var newName = ($(e.target).closest('li').find('.changeNameInput').val())
               tvShowColl.update({'_id':this.tvShowObj._id}, {$set:{name:newName}});
